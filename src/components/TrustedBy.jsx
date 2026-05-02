b39ac4475a60b29eab8d6ea5a962e774c390210b
Syasmi3 <syaasoktaviaa@gmail.com>
2026-05-02 18:05:12 +0700

    Trusted By

---
+++ src/components/TrustedBy.jsx
@@ -0,0 +1,18 @@
+export default function TrustedBy() {
+  return (
+    <section className="py-12 text-center">
+      
+      <p className="text-gray-500 mb-6">
+        Trusted by 15,000+ University Student in Indonesia
+      </p>
+
+      <div className="flex justify-center gap-6 flex-wrap">
+        <img src="/logos/ui.png" alt="UI" className="h-10" />
+        <img src="/logos/ugm.png" alt="UGM" className="h-10" />
+        <img src="/logos/itb.png" alt="ITB" className="h-10" />
+        <img src="/logos/binus.png" alt="Binus" className="h-10" />
+      </div>
+
+    </section>
+  );
+}
