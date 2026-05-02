fee0d10389f2340098773f8812beb54153230e7e
Dio Prasetyo <prasetyodio04@gmail.com>
2026-05-02 17:38:23 +0700

    Update Section Hero

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -2,10 +2,6 @@ import React from "react";
 
 export default function HeroSection() {
   return (
-    <section
-      className="relative flex items-center w-[92%] h-[550px] md:w-[95%] md:max-w-[740px] md:h-[550px] md:top-6 md:rounded-[36px]
-          lg:max-w-[960px] lg:h-[550px]
-          xl:w-[1243px] mx-auto rounded-t-[30px] rounded-b-[50px] bg-[#F8F8FF] top-[23px]"
-    ></section>
+    <section className="relative flex items-center w-[540px] h-[1200px] sm:w-[540px] sm:h-[170px] md:w-[880px] md:h-[788px] lg:w-[1312px] lg:h-[988px] mx-auto rounded-t-[30px] rounded-b-[50px] bg-[#F8F8FF] top-[23px]"></section>
   );
 }
