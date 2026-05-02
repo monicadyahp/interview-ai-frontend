ba349ab97e926382e4d86837e6096939b7d9c378
Dio Prasetyo <prasetyodio04@gmail.com>
2026-05-02 16:42:09 +0700

    Update Hero Responsive

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -2,6 +2,10 @@ import React from "react";
 
 export default function HeroSection() {
   return (
-    <section className="relative flex items-center w-[1312px] min-h-screen max-h-[988px] mx-auto rounded-t-[30px] rounded-b-[50px] bg-[#F8F8FF] top-[23px]"></section>
+    <section
+      className="relative flex items-center w-[92%] h-[550px] md:w-[95%] md:max-w-[740px] md:h-[550px] md:top-6 md:rounded-[36px]
+          lg:max-w-[960px] lg:h-[550px]
+          xl:w-[1243px] mx-auto rounded-t-[30px] rounded-b-[50px] bg-[#F8F8FF] top-[23px]"
+    ></section>
   );
 }
