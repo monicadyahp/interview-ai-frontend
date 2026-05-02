d7483bdd6c53a8cd0f3c04107fc2adf6679c4c9c
Dio Prasetyo <prasetyodio04@gmail.com>
2026-05-02 23:41:32 +0700

    Update Height & With Responsif

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -6,8 +6,9 @@ export default function HeroSection() {
     <section className="relative mx-auto overflow-hidden w-[92%] h-[760px] md:w-[95%] md:w-[90%] lg:w-[90%] md:h-[980px] top-[23px] lg:h-[1020px] rounded-[30px] md:rounded-[40px] bg-[#F8F8FF] shadow-[0_10px_40px_rgba(140,94,173,0.08)]">
       {/* Background Layer - Diperbaiki agar tidak berantakan */}
       <div
-        className="absolute left-0 top-0 w-full z-0 opacity-80"
+        className="absolute top-0 z-0 opacity-80"
         style={{
+          width: "110%",
           height: "110%",
           backgroundImage: "url('/hero/Rectangle.png')",
           backgroundSize: "100% 100%",
