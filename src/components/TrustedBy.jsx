6c9abe243d955c2c541bb84a564ac0278fe44957
Syasmi3 <syaasoktaviaa@gmail.com>
2026-05-09 14:54:14 +0700

    improve TrustedBy section

--- src/components/TrustedBy.jsx
+++ src/components/TrustedBy.jsx
@@ -1,7 +1,6 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay } from "swiper/modules";
 import "swiper/css";
-import "swiper/css/autoplay";
 
 export default function TrustedBy() {
   const logos = [
@@ -20,25 +19,55 @@ export default function TrustedBy() {
   ];
 
   return (
-    <section className="py-16 bg-white text-center">
-      <p className="text-gray-500 mb-8 text-sm">
-        Trusted by 15,000+ University Student in Indonesia
+    <section className="py-24 bg-white text-center overflow-hidden">
+     
+      <p className="text-gray-500 mb-14 text-[18px] font-medium">
+        Trusted by 15,000+ University Students in Indonesia
       </p>
 
-      <div className="max-w-4xl mx-auto">
+      <div className="max-w-[1200px] mx-auto px-6">
         <Swiper
           modules={[Autoplay]}
-          spaceBetween={20}
-          slidesPerView={3}
+          spaceBetween={40}
+          slidesPerView={2}
           loop={true}
-          autoplay={{ delay: 2000 }}
+          speed={3000}
+          autoplay={{
+            delay: 0,
+            disableOnInteraction: false,
+          }}
+          breakpoints={{
+            640: {
+              slidesPerView: 3,
+            },
+            768: {
+              slidesPerView: 4,
+            },
+            1024: {
+              slidesPerView: 5,
+            },
+            1280: {
+              slidesPerView: 6,
+            },
+          }}
         >
           {logos.map((logo, i) => (
             <SwiperSlide key={i}>
-              <img
-                src={logo}
-                className="h-12 mx-auto object-contain"
-              />
+              <div className="flex items-center justify-center h-[120px]">
+                <img
+                  src={logo}
+                  alt="University Logo"
+                  className="
+                    h-20
+                    object-contain
+                    grayscale
+                    hover:grayscale-0
+                    hover:scale-110
+                    transition-all
+                    duration-300
+                  "
+                />
+              </div>
             </SwiperSlide>
           ))}
         </Swiper>
