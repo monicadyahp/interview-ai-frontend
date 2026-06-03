154a3b824c4818c9281860c6db555eed6475f8de
Dio Prasetyo <prasetyodio04@gmail.com>
2026-06-03 20:44:40 +0700

    Update Constant Base API

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,6 @@
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+export const API_BASE_URL =
+  import.meta.env.VITE_API_BASE_URL ||
+  "https://interview-ai-fullstack-backend.vercel.app/api";
 
 export const INTERVIEW_QUESTIONS = [
   "Ceritakan tentang diri Anda...",
